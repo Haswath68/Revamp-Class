dd8b1f07a49c52f99527b71c55d616917928cddd
Haswath68 <haswath68@gmail.com>
2026-06-18 07:19:57 +0000

    server.js updated

--- server.js
+++ server.js
@@ -14,7 +14,6 @@ const app = express();
 const PORT = process.env.PORT;
 const APP = process.env.APP;
 const PASS = "Password";
-const USER = "Username";
 app.get('/' ,  (req, res) => {
 	log("Application is been hit")
 	res.send("Hello from " + APP);
